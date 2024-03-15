31dd0fb3851fa033f92fe1f4260461efa51882b4
k.alshannikava <olshannikovastw@gmail.com>
2024-03-15 13:45:12 +0100

    Remove auto generated classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
 import config from 'config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(config as TypeOrmModuleOptions)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
