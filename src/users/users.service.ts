592c9d45a815970aad00017f0e21ef2f7a0767af
k.alshannikava <olshannikovastw@gmail.com>
2024-03-25 14:23:06 +0100

    Add tests for UserService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { BaseService } from 'src/base/base.service';
+import { BaseService } from '../base/base.service';
 
 @Injectable()
 export class UsersService extends BaseService<User> {
