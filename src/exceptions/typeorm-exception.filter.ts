9d10ae237616f7c88f54814fe9b616c1bd5d2bd3
k.alshannikava <olshannikovastw@gmail.com>
2024-03-28 09:54:53 +0100

    Add exception filter

---
+++ src/exceptions/typeorm-exception.filter.ts
@@ -0,0 +1,22 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { QueryFailedError, TypeORMError } from 'typeorm';
+
+@Catch(TypeORMError, QueryFailedError)
+export class TypeOrmExceptionFilter implements ExceptionFilter {
+  catch(exception: QueryFailedError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = HttpStatus.BAD_REQUEST;
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message,
+    });
+  }
+}
