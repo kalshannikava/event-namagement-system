9e88f89fedbdd50873a5cb574e26ecc4d68a0a4e
k.alshannikava <olshannikovastw@gmail.com>
2024-03-28 09:55:29 +0100

    Use exxception filter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TypeOrmExceptionFilter } from './exceptions/typeorm-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
+  app.useGlobalFilters(new TypeOrmExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
