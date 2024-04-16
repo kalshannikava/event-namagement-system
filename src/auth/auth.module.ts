bf2e087c71f8258c2ec7d872795a4177431fad08
k.alshannikava <olshannikovastw@gmail.com>
2024-04-16 12:45:17 +0200

    Use decorator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
 import { UsersModule } from 'src/users/users.module';
-import { ConfigService } from '@nestjs/config';
+import { JwtAuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [JwtModule.register({}), UsersModule],
@@ -14,6 +17,10 @@ import { ConfigService } from '@nestjs/config';
     AccessTokenStrategy,
     RefreshTokenStrategy,
     ConfigService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AuthModule {}
